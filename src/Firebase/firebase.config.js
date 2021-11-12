eb05991d37343a404a54924cb6054831e3cd050c
Nur-Alom <alamn7150@gmail.com>
2021-11-12 16:37:40 +0600

    first commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSe: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebaseConfig;
